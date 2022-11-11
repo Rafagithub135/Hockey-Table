52df0641dd56b9ca872d392237ee31d087a73a27
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2022-11-10 22:23:41 -0500

    added files js and folders to project

---
+++ js/js-start.js
@@ -0,0 +1,4 @@
+// Write code to open an excel file and display the table on the open-game page.
+
+// Path: js\js-start.js
+// Write code to open an excel file and display the table on the open-game page.
