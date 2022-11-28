2c11ff7e7ce0e545a2a379efaa51222964f59506
Ralph <mccloskey.ralph35@gmail.com>
2022-11-28 18:03:15 -0500

    completed squares

--- js/js-start.js
+++ js/js-start.js
@@ -1,10 +1,8 @@
 "use strict";
-
-    let xlsx = require('xlsx');
-    let workbook = xlsx.readFile('Football_Pool_Sheet_Week_18.xlsx');
-    let sheet_name_list = workbook.SheetNames;
-    let xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    console.log(xlData);
+(function() {
 
 
+
+
+})();
 
