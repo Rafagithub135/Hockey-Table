904155bf4def1a284927334fc38231253bc24ab4
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2022-11-28 22:27:29 -0500

    added code to squares, css and jquery

--- js/blocks.js
+++ js/blocks.js
@@ -1,5 +1,17 @@
-// create a table of 100 blocks
-var blocks = [];
-for (var i = 0; i < 100; i++) {
-    blocks.push(new Block(i));
-}
+// let form = document.getElementById('form');
+//
+//
+// $(document.form.innerHTML).click(); {
+//     if (document.querySelector(".block-visit")) {
+//       document.querySelector(".block-visit").innerHTML = vteam;
+//     }
+//     if (document.querySelector(".block-home")) {
+//       document.querySelector(".block-home").innerHTML = hteam;
+//     }
+//     if (document.querySelector(".playerInfo")) {
+//       document.querySelector(".playerInfo").innerHTML = player;
+//     }
+//     if (document.querySelector("#sqr" === ("choose").toLowerCase())) {
+//         document.querySelector(".block").innerHTML = "#sqr";
+//     }
+// }
