b44e9415695e551b366e8f33635bd93cb4cbf838
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2022-12-16 05:37:13 -0500

    added rapid api code to js file

--- js/blocks.js
+++ js/blocks.js
@@ -15,3 +15,16 @@
 //         document.querySelector(".block").innerHTML = "#sqr";
 //     }
 // }
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'db74cd84e2mshcde8fb146a5e93bp13913ejsn86feef94910d',
+        'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+    }
+};
+
+fetch('https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer&Timezone=-7', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
